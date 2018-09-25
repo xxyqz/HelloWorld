3c7ad5b024a5cf05b0179509641836cfad49f01d
xxyqz <luyan6339@gmail.com>
2018-09-25 15:39:20 +0800

    update settings.py

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'HelloWorld.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR+"/templates",],
+        'DIRS': [BASE_DIR+"HelloWorld/templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
