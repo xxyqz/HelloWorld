f807fef091af42676ab0b1acedfab7d66ce16059
xxyqz <luyan6339@gmail.com>
2018-09-25 16:58:32 +0800

    update admin.py models.py

--- TestModel/admin.py
+++ TestModel/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from TestModel.models import Test
+from TestModel.models import Test,Contact,Tag
 
 # Register your models here.
-admin.site.register(Test)
+admin.site.register([Test, Contact, Tag])
