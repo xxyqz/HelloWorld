20922a4842982bdb3c2ab770482d4af9eae1152c
xxyqz <luyan6339@gmail.com>
2018-09-25 15:29:12 +0800

    add hello.html update settings.py and view.py

--- HelloWorld/view.py
+++ HelloWorld/view.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+# -*- coding: utf-8 -*-
+
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def hello(request):
-    return HttpResponse("Hello world ! ")
+    context = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
