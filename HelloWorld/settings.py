d8495ec0a1a39dfa2e5422cc224e0680a43eddaf
xxyqz <luyan6339@gmail.com>
2018-09-25 15:57:44 +0800

    update settings.py and models.py

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TestModel',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'HelloWorld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'root',
+        'USER': 'root',
+        'PASSWORD': 'Ecust@651018',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- TestModel/models.py
+++ TestModel/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+
+class Test(models.Model):
+    name = models.CharField(max_length=20)
 # Create your models here.
