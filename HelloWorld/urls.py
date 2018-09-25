4be167e9500216689756a3bc1e140d6b026a8ee3
xxyqz <luyan6339@gmail.com>
2018-09-25 16:15:23 +0800

    update urls.py

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from . import view
 
 urlpatterns = [
-    path(r'^hello$', view.hello),
-    path(r'^testdb$', view.hello),
+    path('hello/', view.hello),
+    path('testdb/', view.hello),
 ]
